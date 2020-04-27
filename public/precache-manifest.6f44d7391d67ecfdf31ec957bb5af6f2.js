df18daabb7e078cbc015aac482d3a75abd2218a5
nys013 <1119860833@qq.com>
2020-04-27 12:20:04 +0800

    first commit. This is the server of backstage administration project

---
+++ public/precache-manifest.6f44d7391d67ecfdf31ec957bb5af6f2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6d59a19849ba8232021caec5be4232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d016f6e452a568872bf5",
+    "url": "/static/css/2.a2179b9a.chunk.css"
+  },
+  {
+    "revision": "f230e23e25201ae43463",
+    "url": "/static/css/main.0b11c371.chunk.css"
+  },
+  {
+    "revision": "d016f6e452a568872bf5",
+    "url": "/static/js/2.3b4e836f.chunk.js"
+  },
+  {
+    "revision": "d81ceff70872aa6eb652d78a56232e45",
+    "url": "/static/js/2.3b4e836f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f230e23e25201ae43463",
+    "url": "/static/js/main.55d1ee18.chunk.js"
+  },
+  {
+    "revision": "4de0bc09a0310164fe9a",
+    "url": "/static/js/runtime-main.0528a7e7.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  }
+]);
